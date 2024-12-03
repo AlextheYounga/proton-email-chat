8541ada5c0c4f3c294b044ecad30c7359f417e78
Alex Younger <thealexyounger@proton.me>
2024-12-03 12:59:04 -0600

    feat: add handlers

--- src-tauri/src/db.rs
+++
@@ -1,9 +0,0 @@
-mod models;
-use models::email::Email;
-use models::email_metadata::EmailMetadata;
-use tauri_plugin_sql::Migration;
-
-pub fn migrations() -> Vec<Migration> {
-    // Define migrations to set up the database schema
-    return vec![Email::migration(), EmailMetadata::migration()];
-}
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,23 +1,27 @@
-mod db;
+mod models;
+mod handlers;
 use tauri_plugin_sql::Builder as SqlBuilder;
+use tauri_plugin_sql::Migration;
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+
+
+fn migrations() -> Vec<Migration> {
+	// Define migrations to set up the database schema
+	use models::email::Email;
+	use models::email_metadata::EmailMetadata;
+    return vec![Email::migration(), EmailMetadata::migration()];
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
-        .plugin(
-            SqlBuilder::default()
-                .add_migrations("sqlite:database.db", db::migrations())
-                .build(),
-        )
-        .invoke_handler(tauri::generate_handler![greet])
+		.plugin(SqlBuilder::default()
+		.add_migrations("sqlite:database.db", migrations()).build())
+        .invoke_handler(tauri::generate_handler![
+			handlers::controller::greet
+		])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
